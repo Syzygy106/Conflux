58de9afe99dc3c2a1e82cb29cbb9693579219e91
Vladislav <4pex1oh@gmail.com>
2025-09-17 14:29:25 +0700

    chore: untrack generated artifacts, caches, local deploy files; add .gitignore

--- functions-hardhat-starter-kit/contracts/v4/interfaces/IDaemon.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-interface IDaemon {
-  function getRebateAmount(uint256 blockNumber) external view returns (int128);
-  function accomplishDaemonJob() external;
-}
